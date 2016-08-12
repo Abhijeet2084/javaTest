d5a7bffbf94ae668e2a8f7b268175e6624f2121d
root <root@ec2-54-85-62-248.compute-1.amazonaws.com>
2016-08-12 09:46:58 +0000

    Initial Commit

---
+++ ucp/env.ps1
@@ -0,0 +1,14 @@
+$Env:DOCKER_TLS_VERIFY = "1"
+$Env:DOCKER_CERT_PATH = $(Split-Path $script:MyInvocation.MyCommand.Path)
+$Env:DOCKER_HOST = "tcp://52.202.52.116:443"
+#
+# Bundle for user admin
+# UCP Instance ID DP2N:R4D4:VKPF:CSDH:RGLX:MKD2:W5NB:X2WW:4G4Q:U3HA:2SEI:6NXJ
+#
+# This admin cert will also work directly against Swarm and the individual
+# engine proxies for troubleshooting.  After sourcing this env file, use
+# "docker info" to discover the location of Swarm managers and engines.
+# and use the --host option to override $DOCKER_HOST
+#
+# Run this command from within this directory to configure your shell:
+# Import-Module .\env.ps1
